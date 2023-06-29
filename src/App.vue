a413bf4e046dede63479cab21072a5e6dd823ed9
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-29 13:29:19 +0100

    general-settings

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,27 @@
 
 <template></template>
 
-<style ></style>
+<style>
+/* CSS Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* General */
+body {
+  font-family: 'Open Sans', 'Open Sans Condensed', sans-serif;
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+
+/* Utilities */
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
